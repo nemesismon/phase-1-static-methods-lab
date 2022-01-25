963a3e3e1ef989071834eb19c1f45e5b1f73ee36
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-01-25 16:38:15 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize (string) {
+    let array = string.split('');
+    array[0] = array[0].toUpperCase();
+    let newArr = array.join('');
+    return newArr.toString();
+  }
+  static sanitize (string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+
+  }
+  static titleize (string) {
+    let array = [];
+    let titleArray = [];
+    let cleanTitle;
+    array = string.split(' ');
+
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        let firstWord = array[i];
+        let fwArray = firstWord.split('');
+        fwArray[0] = fwArray[0].toUpperCase();
+        let newString = fwArray.join('');
+        titleArray.push(newString);
+      }
+
+      else {
+        console.log(array[i]);
+        let otherWords = array[i];
+        console.log(i);
+        if (otherWords === 'the' || 'a' || 'an' || 'but' || 'of' || 'and' || 'for' || 'at' || 'by' || 'from') {
+          titleArray.push(array[i]);
+        }
+        else { 
+          let oWord = otherWords.split('');
+          oWord[0] = oWord[0].toUpperCase();
+          let owString = oWord.join('');
+          titleArray.push(owString);
+        }
+      }
+      
+      return cleanTitle = titleArray.join();
+    }
+  }}
